f20b2c844748b69e70129dd8f71d5ce159a8f896
AidedAtom <140497558+AidedAtom@users.noreply.github.com>
2024-02-24 14:09:57 +0200

    Add files via upload
    fahrenheit to celsius calculator

---
+++ ftc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+cout<<"Fahrenheit to Celsius calculator"<<endl;
+
+for(int i = 0; i<50; i++){
+double f, c;
+cin>>f;
+SetConsoleTextAttribute(hConsole, 15);
+c = (f-32)*0.555556;
+cout<<f<<"F = "<<c<<"C"<<endl;
+}
+
+    return 0;
+}
